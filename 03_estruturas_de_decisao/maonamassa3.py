c3eade81695372f8618d4f7d7be4fbd9f8a66980
helanoalbuquerque1989 <helanoalbuquerque196@gmail.com>
2026-09-24 14:10:02 -0300

    mao na massa 3

---
+++ 03_estruturas_de_decisao/maonamassa3.py
@@ -0,0 +1,9 @@
+opcao = int(input("escolha as opção (1, 2 e 3): "))
+if opcao == 1:
+    print("consultar livro")
+elif opcao == 2:
+    print("realizar emprestimo") 
+elif opcao == 3:
+    print("devolver livro")
+else:
+    print("opção não encontrada")
